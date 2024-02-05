b98d62d6f1036017c8f07415e77ea7e35a324cf4
Håvard Trondsen <haavard.trondsen@sparebank1.no>
2024-02-05 17:05:04 +0100

    Henter targets fra api i frontenden

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import { User } from "../store.ts";
-
+import { Target } from "../components/duel.tsx"
 // Utility function for making API requests
+const BACKEND_HOST = "http://localhost:3000"
 const fetchWithCredentials = async <T>(url: string, method: string, body?: any): Promise<T> => {
   const headers = {
     Accept: 'application/json',
@@ -17,27 +18,31 @@ const fetchWithCredentials = async <T>(url: string, method: string, body?: any):
     config.body = JSON.stringify(body);
   }
 
-  const response = await fetch(url, config);
+  const response = await fetch(`${BACKEND_HOST}url`, config);
 
-  if (response.ok) {
-    return response.json();
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`);
   }
 
   return Promise.reject({ message: response.statusText, status: response.status });
 };
 
 export const login = async (email: string, password: string) => {
-  await fetchWithCredentials('http://localhost:3000/login', 'POST', { email, password });
+  await fetchWithCredentials('/login', 'POST', { email, password });
 };
 
 export const getUser = async (): Promise<User> => {
-  return fetchWithCredentials<User>('http://localhost:3000/auth', 'GET');
+  return fetchWithCredentials<User>('/auth', 'GET');
 };
 
 export const duel = async (targetId: number) => {
-  await fetchWithCredentials('http://localhost:3000/duels', 'POST', { attemptedTargetId: targetId })
+  await fetchWithCredentials('/duels', 'POST', { attemptedTargetId: targetId });
 }
 
 export const logout = async () => {
-  await fetchWithCredentials('http://localhost:3000/login/logout', 'POST');
+  await fetchWithCredentials('/login/logout', 'POST');
+}
+
+export const targets = async (): Promise<Target[]> => {
+  return fetchWithCredentials<Target[]>('/targets', 'GET');
 }
--- src/components/duel.tsx
+++ src/components/duel.tsx
@@ -1,14 +1,35 @@
 import { duel } from "../api/api.ts";
 import { QrScanner } from '@yudiel/react-qr-scanner';
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import QRCode from "react-qr-code";
 import { useUserStore } from "../store.ts";
+import { targets } from '../api/api.ts';
+
+export type Target = {
+  id: number
+  name: string,
+  photoHref: string
+}
 
 export const Duel = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
   const user = useUserStore(state => state.user);
   const [cameraOpen, setCameraOpen] = useState(false);
   const [error, setError] = useState<string | undefined>();
+  const [duelTargets, setDuelTargets] = useState<Target[]>();
+
+  useEffect(async () => {
+    const result = await targets()
+    setDuelTargets(result)
+  }, [])
+
+  const deduceClassName = (i: number) => {
+    switch (i) { // ersj
+      case 0: return "main-target"
+      case 1: return "secondary-target"
+      case 2: return "tertiary-target"
+    }
+  }
 
   const handleDecode = (result: string) => {
     duel(parseInt(result)).catch(e => {
@@ -41,6 +62,25 @@ export const Duel = () => {
           </form>
         </dialog>
       }
+      <div>
+        <h2>Dine mål</h2>
+        {duelTargets &&
+          <div className={'duel-targets'}>
+            {duelTargets.map((target: Target, index: number): ReactNode => {
+              return (
+                <div className={deduceClassName(index)} key={index}>
+                  <h2>Mål #{index + 1}</h2>
+                  <img src={`${target.photoHref}`} alt={target.name} height={124} width={124}/>
+                  <h3>{target.name}</h3>
+                  <button onClick={() => duel(target.id)}>Dueller {target.name}</button>
+                </div>)
+            })}
+          </div>
+        }
+        {!duelTargets &&
+          <div>Laster dine mål..</div>
+        }
+      </div>
     </>
   )
 }
